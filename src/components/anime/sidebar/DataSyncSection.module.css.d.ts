c72a9add3f450af6da0a44bc68f8aebe3cf6f8c8
ikaer <personal>
2026-07-21 11:12:02 +0200

    Provider-parity E1-E4

--- src/components/anime/sidebar/DataSyncSection.module.css.d.ts
+++
@@ -1,9 +0,0 @@
-declare const styles: {
-  readonly "buttonGroup": string;
-  readonly "crawlStats": string;
-  readonly "dataSyncSection": string;
-  readonly "error": string;
-  readonly "importRow": string;
-};
-export = styles;
-
